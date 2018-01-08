5bff58c7572ceb367e17a87940422fb8e10034a3
李田锋 <Administrator@personal>
2018-01-08 16:57:24 +0800

    添加action

---
+++ src/cn/edu/lingnan/shop/action/IndexAction.java
@@ -0,0 +1,10 @@
+package cn.edu.lingnan.shop.action;
+
+/**
+ * 首页相关的数据交互
+ * @author Administrator
+ *
+ */
+public class IndexAction extends BaseAction {
+
+}
---
+++ src/cn/edu/lingnan/shop/action/ProductAction.java
@@ -0,0 +1,12 @@
+package cn.edu.lingnan.shop.action;
+
+/**
+ * 商品action
+ * 负责和商品有关的数据控制交互
+ * @author Administrator
+ *
+ */
+
+public class ProductAction extends BaseAction {
+
+}
