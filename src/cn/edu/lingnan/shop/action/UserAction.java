4faf446754be8bcebdab9a7f5eef6743014faf1d
e <yuan@example.com>
2018-01-08 15:54:56 +0800

    添加了service

---
+++ src/cn/edu/lingnan/shop/action/UserAction.java
@@ -0,0 +1,33 @@
+package cn.edu.lingnan.shop.action;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cn.edu.lingnan.shop.pojo.User;
+import cn.edu.lingnan.shop.service.UserService;
+
+public class UserAction extends BaseAction {
+	
+	@Autowired
+	private UserService userService;
+	
+	private User registerUser;
+	
+	//用户注册
+	public String register() {
+		
+		return SUCCESS;
+	}
+
+	
+	//getter and setter
+	public User getRegisterUser() {
+		return registerUser;
+	}
+
+	public void setRegisterUser(User registerUser) {
+		this.registerUser = registerUser;
+	}
+	
+	
+
+}
